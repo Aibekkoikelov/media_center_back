5119c263071aa0f3a65fb6603bfbc416ca669c67
aikolkoikelov <aibekkoikelovdev@gmail.com>
2024-02-14 22:18:33 +0600

    fix bot

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const bot = require('./bot/index.js')
 const express = require('express');  // Подключаем express
 const cors = require('cors');
 const path = require('path');  // Подключаем path (чтобы работать с путями)
-const cookieParser = require('cookie-parser');  // Подключаем cookie-parser (чтобы работать с куками)
+const cookieParser = require('cookie-parser');
+const TelegramBot = require("node-telegram-bot-api");  // Подключаем cookie-parser (чтобы работать с куками)
 const app = express();  // Создаем экземпляр приложение
 
 let id;
@@ -11,13 +11,15 @@ app.use(express.urlencoded({extended: true}));  // Подключаем body-par
 app.use(express.json());  // Подключаем body-parser для работы с json
 app.use(cookieParser());  // Подключаем cookie-parser для работы с куками
 app.use(cors())
-
-
+const API_KEY = '6986635019:AAGKMyQDiRNQce_xyHHNQRLR1GJwhKk9lDI'
+let bot;
 
 app.listen(3001, () => {
     console.log('Server started on port 3001' );
+   bot = new TelegramBot(API_KEY, {
+        polling: true
 
-
+    });
 });
 bot.on('text', async msg => {
     id = await msg.from.id
