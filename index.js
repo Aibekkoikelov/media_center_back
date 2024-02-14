71cc9fe7bee66e0bb86aab930ccfbe9f48715600
aikolkoikelov <aibekkoikelovdev@gmail.com>
2024-02-14 22:31:29 +0600

    delete bot2

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 const express = require('express');  // Подключаем express
 const cors = require('cors');
-const path = require('path');  // Подключаем path (чтобы работать с путями)
 const cookieParser = require('cookie-parser');
 const TelegramBot = require("node-telegram-bot-api");  // Подключаем cookie-parser (чтобы работать с куками)
 const app = express();  // Создаем экземпляр приложение
 
 let id;
-app.use(express.static(path.join(__dirname, 'public')));  // Подключаем статические файлы
 app.use(express.urlencoded({extended: true}));  // Подключаем body-parser для работы с формами
 app.use(express.json());  // Подключаем body-parser для работы с json
 app.use(cookieParser());  // Подключаем cookie-parser для работы с куками
 app.use(cors())
 const API_KEY = '6986635019:AAGKMyQDiRNQce_xyHHNQRLR1GJwhKk9lDI'
 let bot;
-bot = new TelegramBot(API_KEY, {
-    polling: true
-});
+
 app.listen(3001, () => {
     console.log('Server started on port 3001' );
-
+    bot = new TelegramBot(API_KEY, {
+        polling: true
+    });
 });
-bot.on('text', async msg => {
-    id = await msg.from.id
-})
-app.post('/contact', async(req, res) => {
 
-    console.log(req.body)
+app.post('/contact', async(req, res) => {
     if(!id){
         id = 1965934319
     }
